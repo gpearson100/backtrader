2b250c25267e4ffcb618abd55027e7a2b963865f
G Pearson <gpearson100@gmail.com>
2021-07-07 23:17:03 -0500

    pfft

--- tools/binancedownload.py
+++ tools/binancedownload.py
@@ -1,7 +1,7 @@
 from binance.client import Client
 import pandas as pd
 import numpy as np
-
+import re
 """
 # fetch 1 minute klines for the last day up until now
 klines = client.get_historical_klines("BNBBTC", Client.KLINE_INTERVAL_1MINUTE, "1 day ago UTC")
@@ -13,13 +13,31 @@ klines = client.get_historical_klines("ETHBTC", Client.KLINE_INTERVAL_30MINUTE, 
 klines = client.get_historical_klines("NEOBTC", Client.KLINE_INTERVAL_1WEEK, "1 Jan, 2017")
 
 https://python-binance.readthedocs.io/en/latest/market_data.html#id7
+
+RO Key: K0WjrxGuoxQaPdBzqy6FmFhyLvz7ERSxqgUjEh95NyZEKBj4OYz8tbvTKwjXbs9t
+RW key: xVaBtj3Zc47nA2HL17rvrjmBKPLmRHsbcVBrCTjXAjKg0yeEn7PGMlQ1sh4adldL
+"""
+api_key = "K0WjrxGuoxQaPdBzqy6FmFhyLvz7ERSxqgUjEh95NyZEKBj4OYz8tbvTKwjXbs9t"
+api_secret = "xVaBtj3Zc47nA2HL17rvrjmBKPLmRHsbcVBrCTjXAjKg0yeEn7PGMlQ1sh4adldL"
+
+def testy(d, event):
+    for key in d.keys():
+        if re.match(key, event):
+            yield d[key]
+
+
+client = Client(api_key, api_secret)
+
+d = client.get_all_tickers()
+"""
+print(testy(d,"BNB"))
+x = [d[key] for key in d.keys() if re.match(key, "BNB")]
 """
 
-client = Client('', '')
-def getCandles(_symbol="",_interval="",_startTime="",_endTime=""):
+def getCandles(_symbol="BNBBTC",_interval=Client.KLINE_INTERVAL_15MINUTE,_startTime="",_endTime=""):
     df = pd.DataFrame(columns= ['Open_time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close_time'])
     #candles = client.get_klines(symbol='BTCUSDT', interval=Client.KLINE_INTERVAL_1MINUTE, startTime="a", endTime="b")
-    candles = client.get_klines(symbol=_symbol, interval=_interval, startTime=_startTime, endTime=_endTime)
+    candles = client.get_klines(_symbol)
 
     opentime, lopen, lhigh, llow, lclose, lvol, closetime = [], [], [], [], [], [], []
 
@@ -40,3 +58,5 @@ def getCandles(_symbol="",_interval="",_startTime="",_endTime=""):
     df['Volume'] = np.array(lvol).astype(np.float)
     df['Close_time'] = closetime
     return df
+
+results = getCandles("BNBUSDT")
